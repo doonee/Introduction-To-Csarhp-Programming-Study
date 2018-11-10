ca59f7aa448b67c6497d4cb684c341c80bda6bf3
doonee <d2607@naver.com>
2018-11-10 19:48:55 +0900

    Introduction To Csharp Programming Start

---
+++ 1-2-PrimitiveTypeApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCsharpProgramming
+{
+    class _1_2_PrimitiveTypeApp
+    {
+        public static void Main() {
+            bool b;
+            int i = int.MaxValue;
+            uint u = uint.MaxValue;
+            double d = double.MaxValue;
+            b = (i != 0);
+            Console.WriteLine("boolean b = " + b);
+            Console.WriteLine("Max value of signed integer = " + i);
+            Console.WriteLine("Max value of unsigned integer = " + u);
+            Console.WriteLine("Max value of double = " + d);
+        }
+    }
+}
