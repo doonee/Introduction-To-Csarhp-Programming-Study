867795649f2e979afff4f7665f212b891ad18e8b
doonee <d2607@naver.com>
2018-11-11 18:21:12 +0900

    deligate, evnet

---
+++ 1-6-DelegateApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCsharpProgramming
+{
+    class _1_6_DelegateApp
+    {
+        delegate void SampleDelegate();
+
+        class DelegateClass {
+            public void DelegateMethod() {
+                Console.WriteLine("저는 델리게이트 메서드 입니다.");
+            }
+        }
+
+        public static void Main() {
+            DelegateClass dc = new DelegateClass();
+            SampleDelegate sd = new SampleDelegate(dc.DelegateMethod);
+            sd();
+        }
+    }
+}
---
+++ 1-7-EventApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCsharpProgramming
+{
+    class _1_7_EventApp
+    {
+        // [1] 처리하려는 이벤트 처리기를 위한 델리게이트 생성
+        // C# 자체제공 System.EventHandler 사용
+
+        // [2] 이벤트 선언
+        public EventHandler MyEvent;
+
+        // [3] 이벤트 처리기 작성
+        void MyEventHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("안녕하세요!");
+        }
+
+        // [4] 생성자 내에서 이벤트 처리기 등록
+        public _1_7_EventApp() {
+            this.MyEvent += new EventHandler(MyEventHandler);
+        }
+
+        // [5] 이벤트 발생 메서드
+        public void InvokeEvent() {            
+            if (MyEvent != null)
+            {
+                MyEvent(this, null);
+            }
+        }
+
+        public static void Main() {
+            new _1_7_EventApp().InvokeEvent();
+        }
+    }
+}
