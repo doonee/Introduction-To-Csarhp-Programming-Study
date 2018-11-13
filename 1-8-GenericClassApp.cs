e70a78847330514afd84ef3838aac92ff55153d7
doonee <d2607@naver.com>
2018-11-13 22:51:22 +0900

    Generic, Thread

---
+++ 1-8-GenericClassApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCsharpProgramming
+{
+    class Stack<StackType>
+    {
+        private StackType[] stack = new StackType[100];
+        private int sp = -1;
+        public void Push(StackType element)
+        {
+            stack[++sp] = element;
+        }
+        public StackType Pop() {
+            return stack[sp--];
+        }
+    }
+
+    class _1_8_GenericClassApp
+    {
+        public static void Main() {
+            Stack<int> st1 = new Stack<int>();
+            st1.Push(1);
+            st1.Push(2);
+            st1.Push(3);
+            Console.WriteLine(string.Format("{0} {1} {2}", st1.Pop(), st1.Pop(), st1.Pop()));
+            Stack<double> st2 = new Stack<double>();
+            st2.Push(1.5);
+            st2.Push(2.5);
+            st2.Push(3.5);
+            Console.WriteLine(string.Format("{0} {1} {2}", st2.Pop(), st2.Pop(), st2.Pop()));
+        }
+    }
+}
---
+++ 1-9-ThreadApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntroductionToCsharpProgramming
+{
+    class _1_9_ThreadApp
+    {
+        static void ThreadBody() {
+            Console.WriteLine("쓰레드 몸체 실행");
+        }
+
+        public static void Main() {
+            ThreadStart ts = new ThreadStart(ThreadBody);
+            Thread t = new Thread(ts);
+            Console.WriteLine("Start");
+            t.Start();
+            Console.WriteLine("End");
+        }
+    }
+}
